548527a8dd39fd33188b922eeaffa9cfa167352c
ROUSHAN KUMAR CHAUDHARY <121418037+roshanrk44@users.noreply.github.com>
2023-06-30 17:31:30 +0530

    Time: 160 ms (20.47%), Space: 93.2 MB (99.67%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+int ans=INT_MIN;
+    int maxProfit(vector<int>& prices) {
+       int min_price=prices[0];
+       int max_profit=0;
+        for(int i=0;i<prices.size();i++)
+        {
+          min_price=min(min_price,prices[i]);
+          max_profit=max(max_profit,prices[i]-min_price);
+           
+        }
+    
+        return max_profit;
+    }
+};
