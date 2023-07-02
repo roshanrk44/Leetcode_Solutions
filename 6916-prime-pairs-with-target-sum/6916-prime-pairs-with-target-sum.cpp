1be3fc740b996cdcbacd68fd5b64fdf37b52d8a8
ROUSHAN KUMAR CHAUDHARY <121418037+roshanrk44@users.noreply.github.com>
2023-07-02 14:04:01 +0530

    Time: 1495 ms (25.00%), Space: 27.3 MB (100.00%) - LeetHub

---
+++ 6916-prime-pairs-with-target-sum/6916-prime-pairs-with-target-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPrime(int n){
+        if(n==1)
+            return 0;
+        if(n==2||n==3)
+            return 1;
+        if(n%2==0||n%3==0)
+            return 0;
+        for(int i=5;i*i<=n;i+=6){
+            if(n%i==0||n%(i+2)==0)
+                return 0;
+        }
+        return 1;
+    }
+
+    vector<vector<int>> findPrimePairs(int n) {
+         int k=0;
+         vector<vector<int>> ans;
+        for(int i=2;i<=n/2;i++)
+        {
+            if(isPrime(i)==true&&isPrime(n-i)==true)
+            {
+            ans.push_back({i,n-i});
+            }
+            
+        }
+        return ans;
+    }
+};
